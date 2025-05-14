9577241b29de8a333dc0270f430691b8238ab626
harini <harinikovoori@gmail.com>
2025-05-14 12:17:47 +0900

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+// PROMISES METHOD
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+        
+    }
+}
+
+
+export {asyncHandler}
+
+//TRY-CATCH METHOD :
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn (req, res, next)
+    } catch (error) {
+        res.status( error.code || 500 ).json({
+            success : false,
+            message : error.message,
+        })
+    }
+}
+*/ 
