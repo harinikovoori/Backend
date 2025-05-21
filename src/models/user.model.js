f78825d0ec4b576766a3bdda53312156a81be794
harini <harinikovoori@gmail.com>
2025-05-21 17:48:55 +0900

    fixed returning the JWT

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -29,7 +29,7 @@ avatar :{
     required : true,
     unique : true,
 },
-coverimage :{
+coverImage :{
     type : String, //cloudinary url
 },
 password :{
@@ -59,7 +59,7 @@ userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password, this.password)
 }
 userSchema.methods.generateAccessToken = function(){
-    jwt.sign({
+    return jwt.sign({
         _id : this._id,
         email : this.email,
         username : this.username,
@@ -71,7 +71,7 @@ userSchema.methods.generateAccessToken = function(){
    })
 }
 userSchema.methods.generateRefreshToken = function(){
-    jwt.sign({
+    return jwt.sign({
         _id : this._id,
     },
     process.env.REFRESH_TOKEN_SECRET,
